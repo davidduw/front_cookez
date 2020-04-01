7ae01983ec7178cda9fc54127798925662acd76e
Jennifer <jennifer.marsalc@gmail.com>
2020-04-01 17:34:02 +0200

    Display dynamically step's recipe

--- src/app/details-recette/details-recette.page.ts
+++ src/app/details-recette/details-recette.page.ts
@@ -1,9 +1,12 @@
-import { Http } from '@angular/http';
+import { API_TOKEN } from './../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+// import { Http } from '@angular/http';
 import { ModalDetailRecipePage } from './../modal-detail-recipe/modal-detail-recipe.page';
 import { ModalController, NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/map';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-details-recette',
@@ -14,23 +17,40 @@ export class DetailsRecettePage implements OnInit{
 
   information: any [];
   parentPage: string;
+  // parentPage: string = "onglets/accueil-recette";
+  idtype = "";
+  sub;
+  typedata = {};
+  recettes = [];
 
-  constructor(private route: ActivatedRoute, 
+  nom;
+  difficulte;
+  preparation;
+  cuisson;
+  tempsTotal;
+  nbrPersonnes;
+  ingredients;
+  etapesPreparation;
+  etapesCuisson;
+
+
+  constructor(private Activatedroute: ActivatedRoute, 
     private router: Router, 
     private modalController: ModalController,
-    private http: Http,
-    private navCtrl: NavController) {
+    // private http: Http,
+    private navCtrl: NavController, 
+    public http: HttpClient) {
 
-    this.route.queryParams.subscribe(params => {
-      if (this.router.getCurrentNavigation().extras.state) {
-        this.parentPage = this.router.getCurrentNavigation().extras.state.page;
-      }
-    });
+    // this.router.queryParams.subscribe(params => {
+    //   if (this.router.getCurrentNavigation().extras.state) {
+    //     this.parentPage = this.router.getCurrentNavigation().extras.state.page;
+    //   }
+    // });
 
-    let localData = this.http.get('assets/information.json').map(res => res.json().items);
-    localData.subscribe(data => {
-      this.information = data;
-    });
+    // let localData = this.http.get('assets/information.json').map(res => res.json().items);
+    // localData.subscribe(data => {
+    //   this.information = data;
+    // });
   }
 
   async openModal() {
@@ -48,8 +68,76 @@ export class DetailsRecettePage implements OnInit{
     this.information[i].children[j].open = !this.information[i].children[j].open;
   }
 
-  
-
   ngOnInit() {
+    // this.nom = "poulet";
+    this.sub=this.Activatedroute.paramMap.subscribe(params => { 
+      console.log(params);
+       this.idtype = params.get('idtype'); 
+    });
+
+    if(this.idtype != "0"){
+      
+      /* Paramètrage du header */
+      var httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization' : 'BEARER '+ API_TOKEN
+        })
+      }
+
+      /* Affichage recette sélectionnée */
+
+      // this.http.get("http://localhost:8000/api/types/"+this.idtype, httpOptions)
+      this.http.get("http://localhost:3200/api/recettes/1", httpOptions)
+      .subscribe(data => {
+        // titre de la recette
+        this.nom = data['nom'];
+        // difficulté de la recette
+        this.difficulte = data['difficulte'];
+        switch (this.difficulte) {
+          case 1:
+            this.difficulte = "Facile";
+            break;
+          case 2: 
+            this.difficulte = "Moyen";
+            break;
+          case 3:
+            this.difficulte = "Difficile";
+            break;    
+        }
+        // temps total
+        this.preparation = data['tempsprepa'];
+        this.cuisson = data['tempscuisson'];
+        this.tempsTotal = parseInt(this.preparation) + parseInt(this.cuisson) + "min";
+
+        // nombre de personnes
+        this.nbrPersonnes = data['nbrPersonnes'] + " pers.";
+
+        //ingrédients de la recette
+        this.ingredients = data['ingredients'];
+
+        // étapes de préparation
+        this.etapesPreparation = data['preparations'];
+
+         // étapes de cuisson
+         this.etapesCuisson = data['cuissons'];
+
+      }, error => {
+        console.log(error);
+      });
+
+      
+      // this.http.get("http://localhost:8000/api/types/"+this.idtype, httpOptions)
+      // this.http.get("http://localhost:3200/api/etapes", httpOptions)
+      // .subscribe(data => {
+      //   this.etapes = data;
+      //   this.etapes.forEach(etape => {
+      //     const recetteEtape = etape['recette']['id']; 
+      //   });
+      // }, error => {
+      //   console.log(error);
+      // });
+    }
   }
 }
