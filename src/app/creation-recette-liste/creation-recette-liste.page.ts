dc4d580b30b711a648beb0f7b8c9f0f68a1e30c0
David Duwiquet <david.duwiquet@gmail.com>
2020-05-05 01:52:44 +0200

    Avancement page recettes utilisateur + delete recette

--- src/app/creation-recette-liste/creation-recette-liste.page.ts
+++ src/app/creation-recette-liste/creation-recette-liste.page.ts
@@ -1,6 +1,10 @@
 import { NavController, ModalController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { ModalCreateRecipePage } from '../modal-create-recipe/modal-create-recipe.page';
+import { Storage } from '@ionic/storage';
+import { environment, API_TOKEN, TOKEN_KEY, BACK_URL } from '../../environments/environment';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-creation-recette-liste',
@@ -8,17 +12,69 @@ import { ModalCreateRecipePage } from '../modal-create-recipe/modal-create-recip
   styleUrls: ['./creation-recette-liste.page.scss'],
 })
 export class CreationRecetteListePage implements OnInit {
+  token;
+  httpOptions;
+  userMail;
+  recettes = [];
 
-  constructor(private nav: NavController, private modalController: ModalController) { }
+  constructor(private nav: NavController, private router: Router, public http: HttpClient, private modalController: ModalController, private storage: Storage) { }
+
+  ngOnInit() {
+
+    /** Verification si connectée */
+    this.storage.get(TOKEN_KEY).then((value) => {
+      console.log(value);
+      if (value != null) {
+        this.token = value;
+
+        /* Paramètrage du header */
+        this.httpOptions = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'BEARER ' + this.token
+          })
+        }
+
+        this.storage.get("userinfos").then(res => {
+          this.userMail = res['email'];
+        });
+    
+        /* Requete permettant de récuperer les recettes utilisateur */
+        this.http.get(BACK_URL + "/api/users", this.httpOptions)
+        .subscribe(data => {
+          for (const value in data) {
+            if(data[value].email == this.userMail) {
+              this.recettes = data[value].recettes;
+              console.log(this.recettes);
+            }
+          }
+        });
+      } else {
+        this.router.navigate(['/login'])
+      }
+    });
+  }
 
   async openModalCreateRecipe() {
     const modal = await this.modalController.create({
       component: ModalCreateRecipePage
     });
+    modal.onDidDismiss().then((data) => {
+      this.ngOnInit();
+  });
     modal.present();
   }
 
-  ngOnInit() {
+  editRecette(idRecette){
+    console.log('Edition recette ' + idRecette);
   }
 
+  deleteRecette(idRecette){
+    console.log('Supression recette ' + idRecette);
+    this.http.delete(BACK_URL + "/api/recettes/" + idRecette, this.httpOptions)
+        .subscribe(data => {
+          this.ngOnInit();
+        });
+  }
 }
--- src/app/creation-recette/creation-recette.page.ts
+++ src/app/creation-recette/creation-recette.page.ts
@@ -1,15 +1,50 @@
 import { ModalController, NavController } from '@ionic/angular';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalCreateRecipePage } from '../modal-create-recipe/modal-create-recipe.page';
+import { Storage } from '@ionic/storage';
+import { environment, API_TOKEN, TOKEN_KEY, BACK_URL } from '../../environments/environment';
+import { HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-creation-recette',
   templateUrl: 'creation-recette.page.html',
   styleUrls: ['creation-recette.page.scss']
 })
-export class CreationRecettePage {
+export class CreationRecettePage implements OnInit {
+  token;
+  httpOptions;
 
-  constructor(private nav: NavController, private modalController: ModalController) {}
+  constructor(private nav: NavController, private router: Router, private modalController: ModalController, private storage: Storage) {}
+
+  ngOnInit() {
+
+    /** Verification si connectée */
+    this.storage.get(TOKEN_KEY).then((value) => {
+      console.log(value);
+      if (value != null) {
+        this.token = value;
+
+        /* Paramètrage du header */
+        this.httpOptions = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'BEARER ' + this.token
+          })
+        }
+      } else {
+        this.router.navigate(['/login'])
+      }
+    });
+
+    this.storage.get("userinfos").then(res => {
+      console.log(res);
+      if(res['recettes'].length != 0){
+        this.router.navigate(['accueil/onglets/creation-recette-liste']);
+      }
+    });
+  }
 
   async openModalCreateRecipe() {
     const modal = await this.modalController.create({
